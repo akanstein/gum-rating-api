2c02a1ed4d52cf8cbdb849060c751e855d30a047
David Akpan <akanstein@Davids-MacBook-Pro.local>
2021-09-13 12:37:02 +0100

    env for db_name

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: "gum",
+  database: process.env.DB_NAME,
 });
 
 module.exports = connection;
